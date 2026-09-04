05a727ea3da063577aea1747fa06748e28286c4c
Crend King <975235+CrendKing@users.noreply.github.com>
2026-09-03 18:18:49 -0700

    Move config to external JSON file

--- Build.py
+++ Build.py
@@ -1,14 +1,14 @@
+import json
 import os
 import subprocess
 
 from glyphsLib import GSFont
 
 if __name__ == '__main__':
-    TRANSFORMATION = (0.9, 0, 0, 1, 0, 0)
-    ADVANCE = TRANSFORMATION[0]
-    OUTPUT_FILE = 'Monaspace Neon Var My.glyphs'
+    with open('My.json') as file:
+        config = json.load(file)
 
-    font = GSFont('sources/Variable Fonts/Monaspace Neon Var.glyphs')
+    font = GSFont(config['source_file'])
 
     new_version_minor = font.versionMinor + 1
     font.customParameters['versionString'] = font.customParameters['versionString'].replace(str(font.versionMinor), str(new_version_minor))
@@ -20,12 +20,15 @@ if __name__ == '__main__':
     new_masters = []
 
     for i, axis in enumerate(font.axes):
-        if axis.name == 'width':
+        if axis.name.lower() == 'width':
             width_axis_index = i
         else:
             new_axes.append(axis)
 
     for instance in font.instances:
+        if instance.customParameters['Axis Location']:
+            instance.customParameters['Axis Location'] = [al for al in instance.customParameters['Axis Location'] if al['Axis'].lower() != 'width']
+
         if 'Wide' not in instance.name:
             if len(instance.axes) > width_axis_index:
                 del instance.axes[width_axis_index]
@@ -43,12 +46,13 @@ if __name__ == '__main__':
     font.instances = new_instances
     font.masters = new_masters
     remaining_master_ids = {master.id for master in font.masters}
+    TRANSFORMATION = (config['width'], 0, 0, config['height'], 0, 0)
 
     for glyph in font.glyphs:
         glyph.layers = [layer for layer in glyph.layers if (layer.associatedMasterId or layer.layerId) in remaining_master_ids]
 
         for layer in glyph.layers:
-            layer.width *= ADVANCE
+            layer.width *= config['advance']
 
             for path in layer.paths:
                 path.applyTransform(TRANSFORMATION)
@@ -56,9 +60,8 @@ if __name__ == '__main__':
             for component in layer.components:
                 component.applyTransformation(TRANSFORMATION[0], TRANSFORMATION[3])
 
-    font.save(OUTPUT_FILE)
+    font.save(config['output_file'])
 
     subprocess.check_call(['gftools', 'builder', 'config.yaml'])
     os.remove('build.ninja')
     os.remove('.ninja_log')
-    os.remove(OUTPUT_FILE)
